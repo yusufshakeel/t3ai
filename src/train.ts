8561e5047da6ca9daf23bb3a1cbd6449c89a54bc
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-08-03 20:57:15 +0530

    Add training agent

---
+++ src/train.ts
@@ -0,0 +1,24 @@
+import fs from 'fs/promises';
+import NoviceTrainer from './trainers/NoviceTrainer';
+import Configs from './configs';
+import { AgentType } from './types/agent.type';
+import { QTable } from './types/qtable.type';
+
+async function trainAgent() {
+  let qTable: QTable = {};
+  const fileName = `t3ai-${Configs.agentType}.json`;
+  const filePath = process.cwd() + '/models/' + fileName;
+
+  console.log('Starting training...');
+
+  if (Configs.agentType === AgentType.NOVICE) {
+    qTable = NoviceTrainer.train();
+  }
+
+  await fs.writeFile(filePath, JSON.stringify(qTable, null, 2));
+
+  console.log(`QTable saved to ${filePath}`);
+  console.log('Training completed!');
+}
+
+trainAgent().catch(console.error);
