a08e1fa5c6ae6d0369c2e5d7a8cafec033d11d7f
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-08-06 17:27:42 +0530

    Update helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,10 @@
-import { PlayerName, Winner } from '../types/game.type';
+import { Board, PlayerGameSymbol, Winner } from '../types/game.type';
 import Configs from '../configs';
+import { State } from '../types/qtable.type';
 
 export const getRandomIndex = (arr: any[]) => Math.floor(Math.random() * arr.length);
 
-export const getReward = (winner: Winner, player: PlayerName) => {
+export const getReward = (winner: Winner, player: PlayerGameSymbol) => {
   if (winner === null) {
     return Configs.drawPoints;
   }
@@ -12,3 +13,9 @@ export const getReward = (winner: Winner, player: PlayerName) => {
   }
   return winner === player ? Configs.winningPoints : Configs.losingPoints;
 };
+
+export const getBoardFromState = (state: State): Board[] =>
+  state.split('').map(c => (c === '-' ? null : c)) as Board[];
+
+export const getStateFromBoard = (board: Board[]): State =>
+  board.map(c => c ?? '-').join('');
