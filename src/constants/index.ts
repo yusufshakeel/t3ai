a19287deb71ded771b17e1a640543b87e738fe84
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-08-06 17:27:30 +0530

    Update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,20 @@
-export const T3AI_NOVICE_MODEL_FILE_NAME = 't3ai-novice.json';
-export const T3AI_BEGINNER_MODEL_FILE_NAME = 't3ai-beginner.json';
+import { AgentType } from '../types/agent.type';
+
+export const T3AI_NOVICE_MODEL_X_FILE_NAME = 't3ai-novice-x.json';
+export const T3AI_NOVICE_MODEL_O_FILE_NAME = 't3ai-novice-o.json';
+export const T3AI_BEGINNER_MODEL_X_FILE_NAME = 't3ai-beginner-x.json';
+export const T3AI_BEGINNER_MODEL_O_FILE_NAME = 't3ai-beginner-o.json';
+export const T3AI_EXPERT_MODEL_X_FILE_NAME = 't3ai-expert-x.json';
+export const T3AI_EXPERT_MODEL_O_FILE_NAME = 't3ai-expert-o.json';
+
+export const xModels = [
+  { filename: T3AI_NOVICE_MODEL_X_FILE_NAME, agentType: AgentType.NOVICE },
+  { filename: T3AI_BEGINNER_MODEL_X_FILE_NAME, agentType: AgentType.BEGINNER },
+  { filename: T3AI_EXPERT_MODEL_X_FILE_NAME, agentType: AgentType.EXPERT }
+];
+
+export const oModels = [
+  { filename: T3AI_NOVICE_MODEL_O_FILE_NAME, agentType: AgentType.NOVICE },
+  { filename: T3AI_BEGINNER_MODEL_O_FILE_NAME, agentType: AgentType.BEGINNER },
+  { filename: T3AI_EXPERT_MODEL_O_FILE_NAME, agentType: AgentType.EXPERT }
+];
