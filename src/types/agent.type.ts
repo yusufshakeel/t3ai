db90c2eac823b4e736552f5606a180dcf9b2851b
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2025-08-06 17:28:04 +0530

    Update types

--- src/types/agent.type.ts
+++ src/types/agent.type.ts
@@ -1,4 +1,5 @@
 export enum AgentType {
     'NOVICE'= 'novice',
     'BEGINNER'= 'beginner',
+    'EXPERT'= 'expert',
 }
--- src/types/game.type.ts
+++ src/types/game.type.ts
@@ -5,6 +5,6 @@ export enum GameSymbol {
 
 export type Board = GameSymbol.O | GameSymbol.X | null;
 
-export type PlayerName = GameSymbol.O | GameSymbol.X;
+export type PlayerGameSymbol = GameSymbol.O | GameSymbol.X;
 
 export type Winner = GameSymbol.O | GameSymbol.X | null;
